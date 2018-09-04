8de978a1b01399b4220406465b2c584653930faf
HP <41843643+TheBob12@users.noreply.github.com>
2018-09-04 16:06:29 +0200

    Cleaned up GcBuildingDensityLevels

--- libMBIN/Source/Models/Structs/GcBuildingDensityLevels.cs
+++ libMBIN/Source/Models/Structs/GcBuildingDensityLevels.cs
@@ -2,7 +2,7 @@
 {
     public class GcBuildingDensityLevels : NMSTemplate
     {
-		public enum BuildingDensityEnum { Dead, Low, Mid, Full, Weird }
+	public enum BuildingDensityEnum { Dead, Low, Mid, Full, Weird }
         public BuildingDensityEnum BuildingDensity;
     }
 }
