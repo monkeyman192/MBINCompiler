684f73afc998e1ff0133f35e38b42283ac413e5c
HolterPhylo <robertled@gmail.com>
2019-12-20 15:13:40 -0500

    - Added missing structs as of 2.24

---
+++ libMBIN/Source/NMS/GameComponents/GcCameraShakeCapturedData.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.NMS.GameComponents
+{
+    [NMS(Size = 0x14, GUID = 0x1B2372A3A018B4AD, NameHash = 0x472B5C1A8B3B1505, Broken = true)]
+    public class GcCameraShakeCapturedData : NMSTemplate
+    {
+    }
+}
---
+++ libMBIN/Source/NMS/GameComponents/GcTerrainOverlayColours.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.NMS.GameComponents
+{
+    [NMS(Size = 0x18, GUID = 0x9FC4D65E7DF7EFBF, NameHash = 0x61E7123A54EFD252, Broken = true)]
+    public class GcTerrainOverlayColours : NMSTemplate
+    {
+    }
+}
