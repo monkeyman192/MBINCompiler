9aefc3e78a758ab32abc13568b7eb5be3a68762b
HolterPhylo <robertled@gmail.com>
2019-12-20 16:00:21 -0500

    - Remaned/updated files

--- libMBIN/Source/NMS/GameComponents/GcCameraShakeCapturedData.cs
+++ libMBIN/Source/NMS/GameComponents/GcCameraShakeCapturedData.cs
@@ -5,8 +5,17 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(Size = 0x14, GUID = 0x1B2372A3A018B4AD, NameHash = 0x472B5C1A8B3B1505, Broken = true)]
+    [NMS(Size = 0x14, GUID = 0x1B2372A3A018B4AD, NameHash = 0x472B5C1A8B3B1505)]
     public class GcCameraShakeCapturedData : NMSTemplate
     {
+        /* 0x000 */ public bool Active;
+
+        [NMS(Size = 0x3, Ignore = true)]
+        public byte[] Padding1;
+
+        /* 0x004 */ public float ShakeStrength;
+        /* 0x008 */ public float ShakeFrequency;
+        /* 0x0C0 */ public float VibrateStrength;
+        /* 0x010 */ public float VibrateFrequency;
     }
 }
