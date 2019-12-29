a4ccc9adab92c30cc016ab55aef85633981bf90f
HolterPhylo <robertled@gmail.com>
2019-12-28 23:32:50 -0500

    - resolved conflict

--- libMBIN/Source/NMS/GameComponents/GcSolarGenerationData.cs
+++ libMBIN/Source/NMS/GameComponents/GcSolarGenerationData.cs
@@ -8,6 +8,5 @@ namespace libMBIN.NMS.GameComponents
     [NMS(Size = 0x8, GUID = 0xEBECDBDBC0F9EFB9, NameHash = 0x62C0729D49C7216D)]
     public class GcSolarGenerationData : NMSTemplate
     {
-        /* 0x00 */ public ulong SolarSeed;
     }
 }
