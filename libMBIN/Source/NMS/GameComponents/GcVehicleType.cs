3dd1f894d71b44952f6b70f64c27cdb3c458128e
HolterPhylo <robertled@gmail.com>
2021-01-28 21:25:53 -0500

    Correcting GcVehicleType.VehicleTypeEnum entries: Bike, Buggy, ... (as per GcVehicleGlobals) instead of Buggy, Bike, ... (as per the exe)

--- libMBIN/Source/NMS/GameComponents/GcVehicleType.cs
+++ libMBIN/Source/NMS/GameComponents/GcVehicleType.cs
@@ -6,7 +6,9 @@ namespace libMBIN.NMS.GameComponents
 	[NMS(Size = 0x4, GUID = 0xD3947F166802A50B, NameHash = 0x46812D05ED43A48D)]
     public class GcVehicleType : NMSTemplate
     {
-		public enum VehicleTypeEnum { Buggy, Bike, Truck, WheeledBike, Hovercraft, Submarine, Mech }
+        //Note: enum in exe shows Buggy, Bike, ... but GcVehicleGlobals.EXML shows that the first entry is the Bike and 2nd the Buggy
+        // 0x7 entries
+        public enum VehicleTypeEnum { Bike, Buggy, Truck, WheeledBike, Hovercraft, Submarine, Mech }
 		public VehicleTypeEnum VehicleType;
     }
 }
