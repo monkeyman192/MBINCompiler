d769e8a5aa0c40f2949c5b69065aabeb7b9084f0
HolterPhylo <robertled@gmail.com>
2021-01-29 13:04:58 -0500

    Created inline enum in GcVehicleGlobals to swap Buggy and Bike entries

--- libMBIN/Source/NMS/GameComponents/GcVehicleType.cs
+++ libMBIN/Source/NMS/GameComponents/GcVehicleType.cs
@@ -6,9 +6,9 @@ namespace libMBIN.NMS.GameComponents
 	[NMS(Size = 0x4, GUID = 0xD3947F166802A50B, NameHash = 0x46812D05ED43A48D)]
     public class GcVehicleType : NMSTemplate
     {
-        //Note: enum in exe shows Buggy, Bike, ... but GcVehicleGlobals.EXML shows that the first entry is the Bike and 2nd the Buggy
+        //Note: enum in exe shows Buggy, Bike, ... but GcVehicleGlobals.MBIN shows that the first entry is Bike and 2nd is Buggy
         // 0x7 entries
-        public enum VehicleTypeEnum { Bike, Buggy, Truck, WheeledBike, Hovercraft, Submarine, Mech }
+        public enum VehicleTypeEnum { Buggy, Bike, Truck, WheeledBike, Hovercraft, Submarine, Mech }
 		public VehicleTypeEnum VehicleType;
     }
 }
