c31a733e454dab1e5d40e9d853a1cc36b35529b6
Thomas Margraff <thomaspmar@protonmail.com>
2022-07-06 16:51:30 -0700

    Removed NMSDescriptions

--- libMBIN/Source/NMS/BaseTypes/Colour.cs
+++ libMBIN/Source/NMS/BaseTypes/Colour.cs
@@ -8,35 +8,26 @@ namespace libMBIN.NMS
     /// by 255 to get the floating point representation used by the game.
     /// </summary>
     [NMS(Alignment = 0x10, Size = 0x10)]
-    [NMSDescription(
-            @"This class is a simple colour one. The values of each field must be between 0 and 1.
-        To convert from the usual representation of a value between 0 and 255, you just need to divide the value
-        by 255 to get the floating point representation used by the game."
-        )]
     public class Colour : NMSTemplate
     {
         /// <summary>
         /// The Red component of the colour.
         /// </summary>
-        [NMSDescription("The Red component of the colour")]
         public float R;
 
         /// <summary>
         /// The Green component of the colour.
         /// </summary>
-        [NMSDescription("The Green component of the colour")]
         public float G;
 
         /// <summary>
         /// The Blue component of the colour.
         /// </summary>
-        [NMSDescription("The Blue component of the colour")]
         public float B;
 
         /// <summary>
         /// The Alpha component of the colour.
         /// </summary>
-        [NMSDescription("The Alpha component of the colour")]
         public float A;
 
 
