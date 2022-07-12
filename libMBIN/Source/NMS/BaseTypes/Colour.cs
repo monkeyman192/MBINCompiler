5f98b0462433445fefe92be37be14f41bb09d721
Thomas Margraff <thomaspmar@protonmail.com>
2022-07-12 06:52:33 -0700

    Adjusted ToString concerns. Added MathHelper.Clamp. Changed constructor to use bytes

--- libMBIN/Source/NMS/BaseTypes/Colour.cs
+++ libMBIN/Source/NMS/BaseTypes/Colour.cs
@@ -1,4 +1,7 @@
-﻿namespace libMBIN.NMS
+﻿using libMBIN.Source.Common;
+using System;
+
+namespace libMBIN.NMS
 {
     /// <summary>
     /// This class is a simple colour one. The values of each field must be between 0 and 1.
@@ -48,10 +51,10 @@
         /// <param name="A">Alpha component of the colour. Value can be anything between 0 and 1.</param>
         public Colour(float R, float G, float B, float A = 1f)
         {
-            this.R = R;
-            this.G = G;
-            this.B = B;
-            this.A = A;
+            this.R = MathHelper.Clampf(R, 0, 1);
+            this.G = MathHelper.Clampf(G, 0, 1);
+            this.B = MathHelper.Clampf(B, 0, 1);
+            this.A = MathHelper.Clampf(A, 0, 1);
         }
 
         /// <summary>
@@ -62,7 +65,7 @@
         /// <param name="G">Green component of the colour. Value can be any whole number between 0 and 255.</param>
         /// <param name="B">Blue component of the colour. Value can be any whole number between 0 and 255.</param>
         /// <param name="A">Alpha component of the colour. Value can be any whole number between 0 and 255.</param>
-        public Colour(int R, int G, int B, int A = 255)
+        public Colour(byte R, byte G, byte B, byte A = 255)
         {
             this.R = R / 255;
             this.G = G / 255;
@@ -74,16 +77,39 @@
 
         /// <summary>
         /// Returns a formatted string for this Color. Values will be printed in NMS-Style, between 0 and 1.
-        /// <br/>Output: Color [A=1, R=0.58, G=0.35, B=0.80]
+        /// <br/>Output: Colour [R: 0.58, G: 0.35, B: 0.80, A: 1]
         /// </summary>
         /// <returns></returns>
         public override string ToString()
         {
-            int a = (int)(this.A * 255);
-            int r = (int)(this.R * 255);
-            int g = (int)(this.G * 255);
-            int b = (int)(this.B * 255);
-            return $"Colour [A={a}, R={r}, G={g}, B={b}]";
+            return this.ToString(true);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="FLOAT"></param>
+        /// <param name="FORMAT"></param>
+        /// <returns></returns>
+        public string ToString(bool FLOAT = true, string FORMAT = "Colour [R: {0}, G: {1}, B: {2}, A: {3}]")
+        {
+            if (FLOAT) return string.Format(FORMAT, this.R, this.G, this.B, this.A);
+
+            var color = this.ToSystemDrawingColor();
+            return string.Format(FORMAT, color.R, color.G, color.B, color.A);
+        }
+
+        /// <summary>
+        /// Creates a System.Drawing.Color from this Colour.
+        /// </summary>
+        /// <returns></returns>
+        public System.Drawing.Color ToSystemDrawingColor()
+        {
+            int r = (int)MathHelper.Clamp(Math.Round((this.R * 255)), 0, 255);
+            int g = (int)MathHelper.Clamp(Math.Round((this.G * 255)), 0, 255);
+            int b = (int)MathHelper.Clamp(Math.Round((this.B * 255)), 0, 255);
+            int a = (int)MathHelper.Clamp(Math.Round((this.A * 255)), 0, 255);
+            return System.Drawing.Color.FromArgb(a, r, g, b);
         }
 
         /// <summary>
@@ -92,11 +118,7 @@
         /// <param name="colour"></param>
         public static implicit operator System.Drawing.Color(Colour colour)
         {
-            int a = (int)(colour.A * 255);
-            int r = (int)(colour.R * 255);
-            int g = (int)(colour.G * 255);
-            int b = (int)(colour.B * 255);
-            return System.Drawing.Color.FromArgb(a, r, g, b);
+            return colour.ToSystemDrawingColor();
         }
 
         /// <summary>
@@ -104,6 +126,16 @@
         /// </summary>
         /// <param name="systemColor"></param>
         public static implicit operator Colour(System.Drawing.Color systemColor)
+        {
+            return FromSystemDrawingColor(systemColor);
+        }
+
+        /// <summary>
+        /// Creates a Colour from a System.Drawing.Color.
+        /// </summary>
+        /// <param name="systemColor"></param>
+        /// <returns></returns>
+        public static Colour FromSystemDrawingColor(System.Drawing.Color systemColor)
         {
             float a = systemColor.A / 255;
             float r = systemColor.R / 255;
