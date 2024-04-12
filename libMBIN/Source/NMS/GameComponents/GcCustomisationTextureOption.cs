f1f47c1291296dffd6d086e540b493ff6c8c600f
HolterPhylo <robertled@gmail.com>
2024-04-12 11:23:47 -0400

    Update for 14035264

--- libMBIN/Source/NMS/GameComponents/GcCustomisationTextureOption.cs
+++ libMBIN/Source/NMS/GameComponents/GcCustomisationTextureOption.cs
@@ -1,16 +1,14 @@
-using libMBIN.NMS.Toolkit;
 using System.Collections.Generic;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(GUID = 0x5BC4F83DDB5CF580, NameHash = 0xCDB7AC6D11C9DF03)]
+    [NMS(GUID = 0x22A2B4B052E5AF4F, NameHash = 0xCDB7AC6D11C9DF03)]
     public class GcCustomisationTextureOption : NMSTemplate
     {
         /* 0x00 */ public NMSString0x10 TextureOptionsID;
         /* 0x10 */ public NMSString0x10 Layer;
         /* 0x20 */ public NMSString0x10 Group;
-        /* 0x30 */ public TkPaletteTexture Palette;
-        /* 0x38 */ public List<NMSString0x20A> Options;
-        /* 0x48 */ public List<NMSString0x20A> Tips;
+        /* 0x30 */ public List<NMSString0x20A> Options;
+        /* 0x40 */ public List<NMSString0x20A> Tips;
     }
 }
